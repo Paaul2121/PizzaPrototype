f25baba55285d969b9b2ef5699dc08b891436fc3
Oliver <olialexander08@gmail.com>
2023-03-10 16:25:51 +0200

    try carrousel

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -56,7 +56,7 @@ let orderSchema = {
 }
     
     
-    let pizzaList = addEl("div", root, "id", "pizzaList");
+    // let pizzaList = addEl("div", root, "id", "pizzaList");
   
   let allPizzas = async (allergenSelected) => {
     let response = await fetch("/api/pizza");
@@ -138,8 +138,28 @@ let  allOptions = async () => {
 
 const loadEvent = () =>{
     
-    allPizzas([]);
+    // allPizzas([]);
     selectButton.addEventListener('click', select);
+
+    const carousel = document.querySelector(".carousel"),
+  firstImg = carousel.querySelectorAll("img")[0],
+  arrowIcons = document.querySelectorAll(".wrapper i");
+
+const showHideIcons = () => {
+  // showing and hiding prev/next icon according to carousel scroll left value
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? "none" : "block";
+};
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+  });
+}); 
 
     
 
